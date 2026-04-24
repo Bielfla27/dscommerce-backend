c0d7dd975fb7a2cea8687e1a5b02ced534f9782d
Bielfla27 <gabibielmft@outlook.com>
2026-04-24 19:47:30 -0300

    Estruturando camadas , criando dto e o método de pesquisa por id (by id)

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -7,21 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dscommerce.dto.ProductDto;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.ProductService;
 
 @RestController
 @RequestMapping(value  = "/products")
 public class ProductController {
 	
 	@Autowired
-	private ProductRepository repository;
+	private ProductService service;
 
-	@GetMapping
-	public String teste() {
-	
-		Optional<Product> result = repository.findById(1L);
-		Product product = result.get();
-		return product.getName();
+	@GetMapping(value = "/{id}")
+	public ProductDto findById(Long id) {
+		return service.findById(id);
 	}
 }
