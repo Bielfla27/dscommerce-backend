d1e2c43bbd6e18895d62af7560c306c51a8ec6e0
Bielfla27 <gabibielmft@outlook.com>
2026-05-12 19:49:53 -0300

    Criando busca de produtos por paginação

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,8 +1,11 @@
 package com.devsuperior.dscommerce.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +25,10 @@ public class ProductController {
 	@GetMapping(value = "/{id}")
 	public ProductDto findById(Long id) {
 		return service.findById(id);
+	}
+	
+	@GetMapping
+	public Page<ProductDto> findAll(Pageable pageable) {
+		return service.findAll(pageable);
 	}
 }
